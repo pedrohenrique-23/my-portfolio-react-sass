0f6b4cf0a51f23fd371d3c3bcb4303623d84011e
Pedro Henrique <pedrosilva10057@gmail.com>
2024-08-01 00:14:51 -0300

    feat: finished styled information cntainer component

--- my-portfolio-react-sass/src/components/InformationContainer.jsx
+++ my-portfolio-react-sass/src/components/InformationContainer.jsx
@@ -1,8 +1,33 @@
+import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from "react-icons/ai"
+
+import "../styles/components/informationcontainer.sass"
+
 const InformationContainer = () => {
 
     return (
-        <div>InformationContainer
-        </div>
+        <section id="information-container">
+            <div className="info-card">
+                <AiFillPhone id="phone-icon" />
+                <div>
+                    <h3>Telefone</h3>
+                    <p>(88) 99311-8650</p>
+                </div>
+            </div>
+            <div className="info-card">
+                <AiOutlineMail id="email-icon" />
+                <div>
+                    <h3>E-mail</h3>
+                    <p>pedrosilva10057@gmail.com</p>
+                </div>
+            </div>
+            <div className="info-card">
+                <AiFillEnvironment id="pin-icon" />
+                <div>
+                    <h3>Localização</h3>
+                    <p>Sobral - CE</p>
+                </div>
+            </div>
+        </section>
     )
 }
 
