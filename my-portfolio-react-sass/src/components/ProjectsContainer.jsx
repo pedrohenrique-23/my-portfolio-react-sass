f48889c0a7168d45af55a7266aee8e6aae816d75
Pedro Henrique <pedrosilva10057@gmail.com>
2024-08-01 16:34:46 -0300

    feat: added content in ProjectsContainer component

--- my-portfolio-react-sass/src/components/ProjectsContainer.jsx
+++ my-portfolio-react-sass/src/components/ProjectsContainer.jsx
@@ -1,7 +1,11 @@
 const ProjectsContainer = () => {
 
     return (
-        <div>ProjectsContainer</div>
+        <section className="projects-container">
+            <h2>Projetos</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem atque blanditiis praesentium, libero consequuntur quaerat obcaecati consequatur accusamus, eius nesciunt repudiandae eligendi ad illo? Quis odio nulla ipsum numquam doloribus?</p>
+            <a href="#" className="btn">Ver Projetos</a>
+        </section>
     )
 }
 
