9f03534c2a25610337681f3260a36ddc7d75bfda
Pedro Henrique <pedrosilva10057@gmail.com>
2024-08-01 16:34:11 -0300

    feat: update MainContent

--- my-portfolio-react-sass/src/components/MainContent.jsx
+++ my-portfolio-react-sass/src/components/MainContent.jsx
@@ -1,7 +1,8 @@
-import "../styles/components/maincontent.sass"
 import AboutContainer from "./AboutContainer"
 import ProjectsContainer from "./ProjectsContainer"
 import TechnologiesContainer from "./TechnologiesContainer"
+
+import "../styles/components/maincontent.sass"
 
 const MainContent = () => {
 
