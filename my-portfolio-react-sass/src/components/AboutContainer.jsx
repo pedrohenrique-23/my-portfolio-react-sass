52fcc5abdbd9d7549e25ec163133b2a36c7524b5
Pedro Henrique <pedrosilva10057@gmail.com>
2024-08-01 16:33:53 -0300

    feat: added content about container

--- my-portfolio-react-sass/src/components/AboutContainer.jsx
+++ my-portfolio-react-sass/src/components/AboutContainer.jsx
@@ -1,7 +1,11 @@
 const AboutContainer = () => {
 
     return (
-        <div>AboutContainer</div>
+        <section className="about-container">
+            <h2>Sobre</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dignissimos doloremque nesciunt sit. Quaerat soluta tempore eius. Non soluta natus ratione optio, eum nisi unde maxime et aut, esse reprehenderit!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae dolore nemo eius labore repellat iste iusto totam amet inventore exercitationem fugit saepe dolores reiciendis esse necessitatibus, quas neque facere. Repellendus!</p>
+        </section>
     )
 }
 
